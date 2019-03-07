22ffab351acf6bfb661608d6e817996fff5c7ac7
suroxdesigns <samibotss@gmail.com>
2019-03-07 14:14:50 +0100

    First Commit

---
+++ Cobalt/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, -2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(949, 604)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(924, 9)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(20, 19)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(904, 13)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(14, 11)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(47, 533)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(63, 69)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(124, 556)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(28, 24)
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(13, 556)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(21, 20)
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = CType(resources.GetObject("PictureBox7.Image"), System.Drawing.Image)
+        Me.PictureBox7.Location = New System.Drawing.Point(46, 534)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(62, 70)
+        Me.PictureBox7.TabIndex = 6
+        Me.PictureBox7.TabStop = False
+        Me.PictureBox7.Visible = False
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(-1, 79)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(165, 451)
+        Me.WebBrowser1.TabIndex = 7
+        Me.WebBrowser1.Url = New System.Uri("file:///C:/browser.html", System.UriKind.Absolute)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Magneto", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(257, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(174, 25)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Select a song."
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Location = New System.Drawing.Point(120, 35)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(117, 13)
+        Me.TextBox1.TabIndex = 9
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.SystemColors.InfoText
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListBox1.Font = New System.Drawing.Font("Rubik", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.SystemColors.Info
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 19
+        Me.ListBox1.Location = New System.Drawing.Point(167, 79)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(472, 437)
+        Me.ListBox1.TabIndex = 10
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(762, 94)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(186, 221)
+        Me.AxWindowsMediaPlayer1.TabIndex = 11
+        Me.AxWindowsMediaPlayer1.Visible = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Location = New System.Drawing.Point(762, 412)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(173, 177)
+        Me.PictureBox8.TabIndex = 12
+        Me.PictureBox8.TabStop = False
+        Me.PictureBox8.Visible = False
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(158, 557)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(568, 45)
+        Me.TrackBar1.TabIndex = 13
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.None
+        '
+        'Timer1
+        '
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = CType(resources.GetObject("PictureBox9.Image"), System.Drawing.Image)
+        Me.PictureBox9.Location = New System.Drawing.Point(92, 32)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(24, 18)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox9.TabIndex = 14
+        Me.PictureBox9.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(947, 601)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cobalt"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox9 As PictureBox
+End Class
---
+++ Cobalt/Form1.vb
@@ -0,0 +1,145 @@
+﻿Imports System
+Imports System.IO
+
+Public Class Form1
+    Dim Cpt As Integer = 0
+    Dim first As Boolean = True
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Files(100) As String
+        Dim Result As String = ""
+        Dim Final As String
+        'Dim itemindex As Int32 = Array.IndexOf(Files, "item test")
+        'Dim itemname As String = Files(itemindex)
+        Label1.BackColor = Color.FromArgb(23, 25, 31)
+        ListBox1.BackColor = Color.FromArgb(23, 25, 31)
+        TrackBar1.BackColor = Color.FromArgb(23, 25, 31)
+        'ListBox1.Items.AddRange(Directory.GetFiles(My.Computer.FileSystem.SpecialDirectories.MyMusic))
+
+        Try
+            For Each foundFile As String In My.Computer.FileSystem.GetFiles(My.Computer.FileSystem.SpecialDirectories.MyMusic)
+
+                Files = foundFile.Split("\\")
+
+
+                For Each element As String In Files
+                    Result = element
+                Next
+
+                If (Result.EndsWith(".mp3")) Then
+                    Final = Result.Replace(".mp3", "")
+                    ListBox1.Items.Add(Final)
+                End If
+
+
+
+            Next
+        Catch ex As Exception
+            MsgBox("Cobalt couldn't find any songs in your default Music directory.", vbCritical)
+        End Try
+        TrackBar1.Minimum = 0
+        Timer1.Interval = 100
+
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+
+
+            Timer1.Start()
+
+        PictureBox7.Visible = True
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+        PictureBox7.Visible = False
+    End Sub
+
+    Private Sub ListBox1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListBox1.DoubleClick
+        AxWindowsMediaPlayer1.URL = My.Computer.FileSystem.SpecialDirectories.MyMusic + "\" + ListBox1.SelectedItem + ".mp3"
+        PictureBox7.Visible = True
+        Label1.Text = ListBox1.SelectedItem
+        Dim file As TagLib.File = TagLib.File.Create(My.Computer.FileSystem.SpecialDirectories.MyMusic + "\" + ListBox1.SelectedItem + ".mp3")
+        PictureBox8.Visible = True
+        If file.Tag.Pictures.Length >= 1 Then
+            Dim bin As Byte() = DirectCast(file.Tag.Pictures(0).Data.Data, Byte())
+            PictureBox8.Image = Image.FromStream(New MemoryStream(bin)).GetThumbnailImage(180, 180, Nothing, System.IntPtr.Zero)
+
+        Else
+            PictureBox8.Visible = False
+        End If
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+        ListBox1.SelectedIndex += +1
+        AxWindowsMediaPlayer1.URL = My.Computer.FileSystem.SpecialDirectories.MyMusic + "\" + ListBox1.SelectedItem + ".mp3"
+        Dim file As TagLib.File = TagLib.File.Create(My.Computer.FileSystem.SpecialDirectories.MyMusic + "\" + ListBox1.SelectedItem + ".mp3")
+        PictureBox8.Visible = True
+        If file.Tag.Pictures.Length >= 1 Then
+            Dim bin As Byte() = DirectCast(file.Tag.Pictures(0).Data.Data, Byte())
+            PictureBox8.Image = Image.FromStream(New MemoryStream(bin)).GetThumbnailImage(180, 180, Nothing, System.IntPtr.Zero)
+
+        Else
+            PictureBox8.Visible = False
+        End If
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+        ListBox1.SelectedIndex += -1
+        Dim file As TagLib.File = TagLib.File.Create(My.Computer.FileSystem.SpecialDirectories.MyMusic + "\" + ListBox1.SelectedItem + ".mp3")
+        PictureBox8.Visible = True
+        If file.Tag.Pictures.Length >= 1 Then
+            Dim bin As Byte() = DirectCast(file.Tag.Pictures(0).Data.Data, Byte())
+            PictureBox8.Image = Image.FromStream(New MemoryStream(bin)).GetThumbnailImage(180, 180, Nothing, System.IntPtr.Zero)
+
+        Else
+            PictureBox8.Visible = False
+        End If
+        AxWindowsMediaPlayer1.URL = My.Computer.FileSystem.SpecialDirectories.MyMusic + "\" + ListBox1.SelectedItem + ".mp3"
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            TrackBar1.Maximum = CInt(AxWindowsMediaPlayer1.currentMedia.duration.ToString)
+
+            TrackBar1.Value = CInt(AxWindowsMediaPlayer1.Ctlcontrols.currentPosition)
+        Catch ex2 As Exception
+            Timer1.Stop()
+            If first Then
+                MsgBox("No song to play", vbCritical)
+                first = False
+            End If
+
+        End Try
+
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+        AxWindowsMediaPlayer1.Ctlcontrols.currentPosition = CDbl(TrackBar1.Value)
+        System.Threading.Thread.Sleep(300)
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+    End Sub
+
+    Private Sub PictureBox9_Click(sender As Object, e As EventArgs) Handles PictureBox9.Click
+        Dim Search As String = TextBox1.Text
+        Dim IndexItem As Integer = ListBox1.FindString(Search)
+        If (IndexItem <> -1) Then
+            ListBox1.SetSelected(IndexItem, True)
+            'Label1.Text = Search
+        Else
+            MsgBox("Couldn't find song", vbCritical)
+        End If
+    End Sub
+End Class
